3c00e3883444e3a3e05fde53dc9fe3f11240086d
astronaut-nemo <astronautnemo@gmail.com>
2024-08-03 10:03:53 +0300

    intial commit, demo

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float spawnRate = 2.0f;
+    public bool isSpawnApple = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnApples());
+    }
+
+    IEnumerator SpawnApples() {
+        while(isSpawnApple) {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(applePrefab);
+        }
+    }
+}
---
+++ Assets/Scripts/AppleTarget.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTarget : MonoBehaviour
+{
+    // Vector3 applePosition;
+    private float xRange = 3.5f;
+    private float ySpawnPosition = -2;
+    float speed = 12f;
+    public Rigidbody appleRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        appleRb = GetComponent<Rigidbody>(); // get the rigidbody component on the apple without having to access it
+
+        appleRb.AddForce(RandomForce(), ForceMode.Impulse);
+        transform.position = RandomSpawnPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Detroy apple on click
+    private void OnMouseDown() {
+        Destroy(gameObject);
+    }
+
+
+    // Generate random position
+    private Vector3 RandomSpawnPosition() {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPosition, -4);
+    }
+
+    // Generate random force
+    private Vector3 RandomForce() {
+        return Vector3.up * Random.Range(speed, speed+4);
+    }
+}
